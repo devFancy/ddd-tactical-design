1ebe64bec5eeb87b41462cdcb0e21d4e821f093d
devfancy <fancy.junyongmoon@gmail.com>
2025-04-27 10:25:07 +0900

    fix(menu): 트랜잭션 커밋 후 메뉴 상태 갱신 시점 문제 해결
    - 상품 가격 변경 시 메뉴 상태 갱신을 별도 트랜잭션으로 처리
      - 이벤트 리스너가 기존 트랜잭션과 독립적으로 동작해 트랜잭션 커밋 타이밍 문제를 해결함

--- src/main/java/kitchenpos/menus/tobe/application/MenuService.java
+++ src/main/java/kitchenpos/menus/tobe/application/MenuService.java
@@ -4,6 +4,7 @@ import kitchenpos.menus.tobe.domain.Menu;
 import kitchenpos.menus.tobe.domain.MenuRepository;
 import kitchenpos.products.tobe.domain.event.ProductPriceChangedEvent;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -19,8 +20,10 @@ public class MenuService {
         this.menuRepository = menuRepository;
     }
 
-    @Transactional // ProductService와 별도로 새로운 트랜잭션으로 시작
-    @TransactionalEventListener // default: AFTER_COMMIT -> 트랜잭션 커밋 후 실행
+    // 상품 가격 변경 트랜잭션이 커밋된 후,
+    // 메뉴 업데이트 트랜잭션이 새로 열려서 안정적으로 동작함.
+    @Transactional(propagation = Propagation.REQUIRES_NEW) // 새로운 트랜잭션 시작
+    @TransactionalEventListener() // default: AFTER_COMMIT -> 트랜잭션 커밋 후 실행
     public void handleProductPriceChanged(final ProductPriceChangedEvent event) {
         final List<Menu> menus = menuRepository.findAllByProductId(event.productId());
 
